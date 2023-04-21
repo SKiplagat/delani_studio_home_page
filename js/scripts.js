766ad7120aef4de3c40cfaadf21dbc807e2d2e0a
Simeon Kiplagat <simeon.kiplagat@student.moringaschool.com>
2023-04-21 19:32:31 +0300

    Added jquery fuctions for clickable

---
+++ js/scripts.js
@@ -0,0 +1,16 @@
+$(document).ready(function() {
+
+    $(".clickable1").click(function() {
+      $(".image1").toggle();
+      $(".whatWeDo1").toggle();
+    });
+    $(".clickable2").click(function() {
+        $(".image2").toggle();
+        $(".whatWeDo2").toggle();
+      });
+      $(".clickable3").click(function() {
+        $(".image3").toggle();
+        $(".whatWeDo3").toggle();
+      });
+  
+});
